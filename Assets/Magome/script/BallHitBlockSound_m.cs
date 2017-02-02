0ba9e30741a0d94cf9d227653c6a95a126b2a71b
magosuke <montblanc.lgador@gmail.com>
2017-02-02 17:13:59 +0900

    まごめです

---
+++ Assets/Magome/script/BallHitBlockSound_m.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallHitBlockSound_m : MonoBehaviour {
+	public AudioClip audioClip;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		audioSource.clip = audioClip;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "block") {
+			audioSource.PlayOneShot (audioClip);
+		}
+	}
+}
---
+++ Assets/Magome/script/Title_m.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Title_m : MonoBehaviour {
+	public Text blinktext;
+	// "public" can create window to put text on Unity 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKeyDown) {
+			Application.LoadLevel ("main");
+			// this make player move to any scene with tag
+		}
+
+		blinktext.color = new Color (1, 1, 1, Mathf.PingPong (Time.time, 1));
+	}
+}
